f67d1584661a1c65f2c4172145c9aa9e5b611748
KevinNtomeno <ndombele949@gmail.com>
2025-07-06 22:45:18 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,21 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,       // nome do banco
-  process.env.DB_USER,       // usuário
-  process.env.DB_PASSWORD,   // senha
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    logging: false
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 module.exports = sequelize;
