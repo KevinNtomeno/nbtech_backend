d4a895b9e08e1c0d975a2bddead5c418745a8d22
KevinNtomeno <ndombele949@gmail.com>
2025-07-06 22:48:49 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,13 +1,22 @@
+// força o Node a priorizar IPv4 em vez de IPv6
+const dns = require('dns');
+if (dns.setDefaultResultOrder) {
+  dns.setDefaultResultOrder('ipv4first');
+}
+
 const { Sequelize } = require('sequelize');
+
+if (!process.env.DATABASE_URL) {
+  console.error('❌ DATABASE_URL não definida!');
+  process.exit(1);
+}
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+    ssl: { require: true, rejectUnauthorized: false }
+  },
+  logging: false
 });
 
 module.exports = sequelize;
